f32646accaf850573b3c8940a81fceaf9fd4cf2e
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-02-16 09:11:58 +0000

    undo changes

--- models/base_model.py
+++ models/base_model.py
@@ -27,18 +27,15 @@ class BaseModel():
             *args: unused.
             **kwargs (dict): a dictionary containing wanted attributes.
         """
-        fmt = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
         if kwargs is not None and kwargs != {}:
             for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 if k != "__class__":
                     setattr(self, k, v)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], fmt)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], fmt)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
