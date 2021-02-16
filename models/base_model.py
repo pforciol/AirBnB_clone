1d621db271244075435c8ddea5db2f39255b049a
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-02-16 09:11:00 +0000

    fix(): try to fix something

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,6 @@ class BaseModel():
             **kwargs (dict): a dictionary containing wanted attributes.
         """
         fmt = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
         if kwargs is not None and kwargs != {}:
             for k, v in kwargs.items():
                 if k != "__class__":
@@ -39,6 +37,8 @@ class BaseModel():
             if hasattr(self, "updated_at") and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], fmt)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
