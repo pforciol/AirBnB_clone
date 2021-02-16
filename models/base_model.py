35c7bf74d3d126bef6f351927eec488fedd3ba45
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-02-16 09:17:08 +0000

    Trying with Matt's BaseModel.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,57 +1,50 @@
 #!/usr/bin/python3
-"""This is the Base Model module.
-
-Contains the BaseModel class which will be the
-"base" of all other classes in this project.
 """
+Definition of Class : BaseModel
+"""
+
 import uuid
 from datetime import datetime
 import models
 
+fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
-class BaseModel():
-    """This class will be the “base” of all other classes in this project.
 
-    The goal is to manage all common attributes and methods for other classes.
-
-    Attributes:
-        id (str): unique random ID for each BaseModel instance.
-        created_at (datetime): the current datetime when instance is created.
-        updated_at (datetime): the current datetime when instance is updated.
-    """
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes the default attributes of the BaseModel object.
-
-        Args:
-            *args: unused.
-            **kwargs (dict): a dictionary containing wanted attributes.
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        if kwargs is not None and kwargs != {}:
-            for k, v in kwargs.items():
-                if k in ["created_at", "updated_at"]:
-                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                if k != "__class__":
-                    setattr(self, k, v)
+        """Initialization of Base Model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], fmt)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], fmt)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
-        """Overrides the default behaviour of the __str__ method."""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """String method for Base Model"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
+        """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary with all the keys/values of the instance."""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """Returns a dictionary containing  __dict__ of the instance"""
+        mydict = self.__dict__.copy()
+        mydict["__class__"] = self.__class__.__name__
+        if "created_at" in mydict.keys():
+            mydict["created_at"] = mydict["created_at"].strftime(fmt)
+        if "updated_at" in mydict.keys():
+            mydict["updated_at"] = mydict["updated_at"].strftime(fmt)
+        return mydict
